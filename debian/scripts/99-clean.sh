7f7d318d413c4f4feba907eb4261168d1d372b07
jeanluc <lkubb@protonmail.com>
2024-01-24 01:33:05 +0100

    Minor Debian fix
    For some reason, dhclient always runs unless /etc/network/interfaces
    does not contain a `dhcp` interface at all.

--- debian/scripts/99-clean.sh
+++ debian/scripts/99-clean.sh
@@ -13,14 +13,16 @@ find /var/log/ -name *.log -exec rm -f {} \;
 rm -f /root/.bash_history
 # Replace DNS config from DHCP with something generic.
 # You will need to manage this file somehow on its children.
-# cloud-init network config adds nameservers
-# using `dns-nameservers 8.0.0.8` in /etc/network/interfaces.d,
-# which relies on resolvconf or NetworkManager (which is why it
-# works on RHEL family by default).
+# For stock Debian Server and ifupdown, you'll need resolvconf
+# or do something with cloud-init.
 cat << EOF > /etc/resolv.conf
 nameserver 1.1.1.1
 nameserver 8.8.8.8
 EOF
+
+# Try to disable dhclient from running by default,
+# it does this even if the configuration is overridden
+sed -i 's/dhcp/manual/' /etc/network/interfaces
 
 dd if=/dev/zero of=/EMPTY bs=1M || true
 rm -f /EMPTY
